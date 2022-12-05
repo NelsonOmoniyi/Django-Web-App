af159c5f40c9839eb8bf236da7714004d8850967
NelsonOmoniyi <omoniyinelson6@gmail.com>
2022-12-05 08:46:20 +0100

    Completed Django Module 05/12/2022

--- nelson/tasks/urls.py
+++ nelson/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views 
 
+app_name = "tasks"
 urlpatterns = [
-    path("", views.task, name='task')
+    path("", views.task, name='task'),
+    path("add", views.add, name='add')
 ]
--- nelson/tasks/views.py
+++ nelson/tasks/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value="5", max_value="8")
 
 # Create your views here.
-tasks = ["Nelson", "Precious", "Love"]
+tasks = []
 def task(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "task/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:task"))
+        else:
+            return render(request, "task/add.html", {
+                "form": form
+            })
+
+
+    return render(request, "task/add.html", {
+        "form": NewTaskForm()
     })
