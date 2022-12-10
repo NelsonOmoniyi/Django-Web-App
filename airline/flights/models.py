3ff3de72d00eaf3015f4d70849973d824738efbc
Nelson Omoniyi <omoniyinelson6@gmail.com>
2022-12-10 21:27:06 +0100

    Add templates folder and add extra functionalities to the flight module

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.code} -- {self.city}"
+
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="depatures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Flight, Airport
 # Create your views here.
+
+def flights(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
