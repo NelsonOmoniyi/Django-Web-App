54ea555c39fd4e8c721fb6aaa604b8ac1f774f99
Nelson Omoniyi <omoniyinelson6@gmail.com>
2022-12-11 01:37:57 +0100

    edit views.py

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Flight, Airport
 # Create your views here.
 
+
 def flights(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
